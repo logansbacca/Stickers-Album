b59633419fc6a8869a51a15ebb72eb484f1afe15
Logan <logans.bacca@gmail.com>
2023-02-20 15:58:35 +0100

    commit

--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,13 @@
 import { logOut } from "../modules/logout";
 let signOut = document.getElementById("sign-out");
+let join = document.getElementById("join");
 
 signOut.addEventListener("click", () => {
   logOut();
+});
+
+
+
+join.addEventListener("click", () => {
+  window.location.href = "../register/index.html";
 });
